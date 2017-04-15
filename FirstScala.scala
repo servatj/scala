fa7aa1f45db73fdb1cd69580f410c27691fb0232
Josep <servatj@Joseps-MacBook-Pro.local>
2017-04-15 20:09:50 +0200

    FirstScala program

--- FirstScala.scala
+++ FirstScala.scala
@@ -17,9 +17,35 @@ object FirstScala {
       	b += 1 
       } while(b <= 20)
 
-      
+      var c = 0
+
       for (c <- 1 to 10){
       	println(c)
+      }
+
+      val abecedary = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+      for (i <- 0 until abecedary.length) {
+      	 println(i)
+      }
+
+      val list = List(2,3,4,5)
+
+      for (i <- list){
+      	println("item list " + list)
+      }
+
+      var evenList = for { i <- 0 to 20
+        if (i % 2) == 0 
+      } yield i 
+
+      for (i <- evenList) 
+        println(i)
+
+
+      for (i <- 0 to 5; j <- 0 to 10){
+      	println("i : " + i)
+      	println("j : " + j)
       }
 
 
