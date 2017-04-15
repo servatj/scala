b6fef59b5c515d98ec5111c333c95e9053c7f5e8
Josep <servatj@Joseps-MacBook-Pro.local>
2017-04-15 19:29:41 +0200

    This is my first Commit

---
+++ FirstScala.scala
@@ -0,0 +1,18 @@
+object FirstScala {
+	def main(args: Array[String]){
+	  var i = 0 
+
+	  while(i <= 10){
+	  	println(i)
+	  	i += 1
+	  }
+
+	  var b = 0 
+
+      do {
+      	println(b)
+      	b += 1 
+      } while(b <= 20)
+
+	}
+}
